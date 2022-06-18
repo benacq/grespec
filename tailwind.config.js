20c1371c7b06c2470bc5dd192e26edff887538af
benacq <benacq44@gmail.com>
2022-06-18 23:10:17 +0000

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,48 @@
+const {
+  screens,
+  letterSpacing,
+  lineHeight
+} = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+
+    minHeight: (theme) => ({
+      ...theme('spacing'),
+      'custom-min': '100vh'
+    }),
+
+    minWidth: (theme) => ({
+      ...theme('spacing'),
+      'custom-min': '100vw',
+      'custom-small': '150px'
+    }),
+
+    screens: {
+      'xsm': '480px',
+      ...screens
+    },
+
+
     extend: {
 
-      
+      fontSize: {
+        'custom-xxlg': '60px',
+        'custom-xlg': '32px',
+        'custom-lg': '28px',
+        'custom-md': '22px',
+        'custom-md-sm': '20px',
+        'custom-sm': '18px',
+
+        'custom-xsm': '20px',
+        'custom-xxsm': '16px',
+        'custom-xxxsm': '10px'
+      },
+
     },
   },
   plugins: [],
